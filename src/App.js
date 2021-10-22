0a45fe22bdea009b89fa9d7fd05eb42aac429c4f
Dimas Mendoza <damendoza98@gmail.com>
2021-10-22 13:07:07 -0500

    End Challenge

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import "./App.css";
 import Links from "./components/Links";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function App() {
  return (
   <div className="container p-4">
    <div className="row">
     <Links />
    </div>
+   <ToastContainer />
   </div>
  );
 }
